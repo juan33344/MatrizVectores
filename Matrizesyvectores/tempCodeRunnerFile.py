79e170254c9ef969041057aeeb72dfbc15aabc6a
soy Juan <soyjuanjose@gmail.com>
2025-08-14 13:54:05 -0500

    Actualicé el archivo X

---
+++ Matrizesyvectores/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+import numpy as npd
+#definir el arreglo o vector
+a = npd.array([23,45,63,56])
+b = npd.array([45,85,20,58])
+print(a[2])
+
+a[2] = 81
+#operaciones con arrays
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a%b)
